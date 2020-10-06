e1ff52b286ad2d93ef8e43591723babe14b3397b
JoseBurgos1993 <burgosjd1993@gmail.com>
2020-10-06 19:25:37 -0400

    Added a few basic components

---
+++ src/components/Body/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Body() {
+  return (
+    <div className="mainbody">
+      <span className="span1">This is the body</span>
+    </div>
+  );
+}
+
+export default Body;
