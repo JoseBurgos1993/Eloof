57f3f95c149bb3483af9df6ea8267a48ce774bfa
Pha Kpa <kphaoctober@yahoo.com>
2020-10-20 21:54:14 -0400

    updated believer routes

--- client/src/components/BelieverRegistration/index.js
+++ client/src/components/BelieverRegistration/index.js
@@ -1,48 +1,100 @@
-import React, { Component } from 'react';
-import { Button, List } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Button, List } from "semantic-ui-react";
+import axios from "axios";
 
-class BelieverRegistration extends Component{
-    saveAndContinue = (e) => {
-        e.preventDefault();
-        this.props.nextStep();
-    }
+class BelieverRegistration extends Component {
+  saveAndContinue = (e) => {
+    e.preventDefault();
+    /////////////////////////////////////////////////////////////////////////////
 
-    back  = (e) => {
-        e.preventDefault();
-        this.props.prevStep();
-    }
+    const handleLogin = () => {
+      axios
+        .post("/api/users/login", {
+          username: this.props.values.childEmail,
+          password: this.props.values.childPassword,
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            window.location.replace("/");
+          }
+        })
+        .catch((error) => {
+          console.log("login error: ");
+          console.log(error);
+        });
+    };
 
-    render(){
-        const {values: { childName, childAge, childGrade, childLanguage } } = this.props;
+    axios
+      .post("/api/users", {
+        username: this.props.values.childEmail,
+        password: this.props.values.childPassword,
+        usertype: "believer",
+        childName: this.props.values.childName,
+        childAge: this.props.values.childAge,
+        childGrade: this.props.values.childGrade,
+        childLanguage: this.props.values.childLanguage,
+      })
+      .then((response) => {
+        if (!response.data.error) {
+          window.location.replace("/");
+        } else {
+          alert("User ID is already taken!"); //alert user, ID taken
+          console.log("USERNAME TAKEN");
+        }
+      })
+      .then(handleLogin)
+      .catch((error) => {
+        console.log(error);
+      });
 
-        return(
-            <div>
-                <h1 className="ui centered">Confirm you letter to Santa</h1>
-                <p>Click Confirm if you would like to send the following information to Santa</p>
-                <List>
-                    <List.Item>
-                        <List.Icon name='users' />
-                        <List.Content>Name: {childName}</List.Content>
-                    </List.Item>
-                    <List.Item>
-                        <List.Icon name='calendar' />
-                        <List.Content>Age: {childAge} years old</List.Content>
-                    </List.Item>
-                    <List.Item>
-                        <List.Icon name='marker' />
-                        <List.Content>Grade: {childGrade}</List.Content>
-                    </List.Item>
-                    <List.Item>
-                        <List.Icon name='marker' />
-                        <List.Content>Language: {childLanguage}</List.Content>
-                    </List.Item>
-                </List>
+    /////////////////////////////////////////////////////////////////////////////
+  };
 
-                <Button onClick={this.back}>Back</Button>
-                <Button onClick={this.saveAndContinue}>Confirm</Button>
-            </div>
-        )
-    }
+  back = (e) => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
+  render() {
+    const {
+      values: { childName, childAge, childGrade, childLanguage, childEmail },
+    } = this.props;
+
+    return (
+      <div>
+        <h1 className="ui centered">Confirm you letter to Santa</h1>
+        <p>
+          Click Confirm if you would like to send the following information to
+          Santa
+        </p>
+        <List>
+          <List.Item>
+            <List.Icon name="users" />
+            <List.Content>Name: {childName}</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name="calendar" />
+            <List.Content>Age: {childAge} years old</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name="marker" />
+            <List.Content>Grade: {childGrade}</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name="marker" />
+            <List.Content>Language: {childLanguage}</List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name="marker" />
+            <List.Content>Email: {childEmail}</List.Content>
+          </List.Item>
+        </List>
+
+        <Button onClick={this.back}>Back</Button>
+        <Button onClick={this.saveAndContinue}>Confirm</Button>
+      </div>
+    );
+  }
 }
 
 export default BelieverRegistration;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,16 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-const User = require('../../database/models/user');
-const passport = require('../../passport');
+const User = require("../../database/models/user");
+const passport = require("../../passport");
 
-router.post('/', (req, res) => {
-  const { username, password, usertype, wishlist } = req.body;
+router.post("/", (req, res) => {
+  const {
+    username,
+    password,
+    usertype,
+    wishlist,
+    childName,
+    childAge,
+    childGrade,
+    childLanguage,
+  } = req.body;
 
   User.findOne({ username: username }, (err, user) => {
     if (err) {
-      console.log('User Create Error: ', err);
+      console.log("User Create Error: ", err);
       return;
     }
 
@@ -21,22 +30,24 @@ router.post('/', (req, res) => {
       return;
     }
     let newUser;
-    if(usertype === "elf"){
+    if (usertype === "elf") {
       newUser = new User({
         username: username,
         password: password,
         usertype: usertype,
       });
-
-    } else if(usertype === "believer"){
+    } else if (usertype === "believer") {
       newUser = new User({
         username: username,
         password: password,
         usertype: usertype,
         wishlist: wishlist,
+        childName: childName,
+        childAge: childAge,
+        childGrade: childGrade,
+        childLanguage: childLanguage,
       });
-
-    } else{
+    } else {
       console.log("ERRROROOR");
     }
 
@@ -49,22 +60,22 @@ router.post('/', (req, res) => {
 });
 
 router.post(
-  '/login',
+  "/login",
   (req, res, next) => {
     next();
   },
-  passport.authenticate('local'),
+  passport.authenticate("local"),
   (req, res) => {
-    console.log('LOGGED IN', req.user);
+    console.log("LOGGED IN", req.user);
     res.send({
       username: req.user.username,
       usertype: req.user.usertype,
-      wishlist: req.user.wishlist
+      wishlist: req.user.wishlist,
     });
   }
 );
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   if (req.user) {
     console.log("In the api thing, the user is = ", req.user);
     res.json({ user: req.user });
@@ -76,52 +87,55 @@ router.get('/', (req, res) => {
 router.get("/all", (req, res) => {
   User.find((err, results) => {
     if (err) {
-      console.log('User Find Error: ', err);
+      console.log("User Find Error: ", err);
       return;
     }
     res.json(results);
   });
 });
 
-router.post('/logout', (req, res) => {
+router.post("/logout", (req, res) => {
   if (req.user) {
     req.logout();
-    res.status(200).json({ msg: 'LOGGED OUT' });
+    res.status(200).json({ msg: "LOGGED OUT" });
   } else {
-    res.status(404).json({ msg: 'NO USER TO LOGOUT' });
+    res.status(404).json({ msg: "NO USER TO LOGOUT" });
   }
 });
 
 /////////
 
-router.get('/profile', (req,res) => {
+router.get("/profile", (req, res) => {
   if (req.user) {
-    User.findOne({username: req.user.username}, (err, user) => {
-      if(err){
+    User.findOne({ username: req.user.username }, (err, user) => {
+      if (err) {
         console.log("User find Error: ", err);
         return;
       }
       console.log("User ", user);
       res.json({ user: user });
-
     });
   } else {
     res.json({ user: null });
   }
 });
 
-router.post("/additem", (req,res) => {
+router.post("/additem", (req, res) => {
   console.log("The update request: ", req.body);
-  const {item, user} = req.body;
-  console.log("Item in the update part: ", item)
-  User.update({_id: user._id}, {$push: {"wishlist": item.name}}, (err, result) => {
-    if(err){
-      console.log("User Update Error: ", err);
-      return;
+  const { item, user } = req.body;
+  console.log("Item in the update part: ", item);
+  User.update(
+    { _id: user._id },
+    { $push: { wishlist: item.name } },
+    (err, result) => {
+      if (err) {
+        console.log("User Update Error: ", err);
+        return;
+      }
+      console.log("It did the update?");
+      res.status(200);
     }
-    console.log("It did the update?")
-    res.status(200);
-  });
+  );
 });
 
 module.exports = router;
